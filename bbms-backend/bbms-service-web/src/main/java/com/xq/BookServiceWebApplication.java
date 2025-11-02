fedf58575b1627ffd82aabecbbd416a43fa8d3fe
Jessica Wang <1271736670@qq.com>
2025-11-02 22:36:52 +0800

    feat: 整合mybatis-plus

---
+++ bbms-backend/bbms-service-web/src/main/java/com/xq/BookServiceWebApplication.java
@@ -0,0 +1,11 @@
+package com.xq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceWebApplication.class, args);
+    }
+}
