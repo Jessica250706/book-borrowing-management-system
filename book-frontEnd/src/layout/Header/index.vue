c79736f3536941019e333b97cafd5fae7d07a896
lin <528077701@qq.com>
2025-11-07 21:38:58 +0800

    feat：新增头像框及下拉菜单

--- book-frontEnd/src/layout/Header/index.vue
+++ book-frontEnd/src/layout/Header/index.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
-    import Collapse from './components/collapse.vue';
-    import BreadCrumb from './components/BreadCrumb.vue';
+import Collapse from './components/collapse.vue';
+import BreadCrumb from './components/BreadCrumb.vue';
+import UserMenu from './components/UserMenu.vue'; // 新增导入
 </script>
 
 <template>
-    <Collapse />
-    <BreadCrumb />
+  <div class="header-container">
+    <div class="header-left">
+      <Collapse />
+      <BreadCrumb />
+    </div>
+    <UserMenu />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.header-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  
+  .header-left {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
